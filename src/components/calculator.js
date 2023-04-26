900eb2acd3e732bbbe5c2a40825feacea156540c
Oumaima Nejjari <oumanj1998@gmail.com>
2023-04-26 02:48:21 +0100

    Implementing required changes

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,42 +1,35 @@
 import React from 'react';
 import Button from './Buttons';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="calculator-grid">
-        <div className="output">
-          <div className="current-result">
-            0
-          </div>
+function Calculator() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="current-result">
+          0
         </div>
-        <Button value="AC" />
-        <Button value="+/-" />
-        <Button value="%" />
-        <Button value="÷" bg="orange" />
-        <Button value={7} />
-        <Button value={8} />
-        <Button value={9} />
-        <Button value="*" bg="orange" />
-        <Button value={4} />
-        <Button value={5} />
-        <Button value={6} />
-        <Button value="-" bg="orange" />
-        <Button value={1} />
-        <Button value={2} />
-        <Button value={3} />
-        <Button value="+" bg="orange" />
-        <Button value={0} span="span1" />
-        <Button value="." />
-        <Button value="=" bg="orange" />
       </div>
-    );
-  }
+      <Button value="AC" />
+      <Button value="+/-" />
+      <Button value="%" />
+      <Button value="÷" bg="orange" />
+      <Button value={7} />
+      <Button value={8} />
+      <Button value={9} />
+      <Button value="*" bg="orange" />
+      <Button value={4} />
+      <Button value={5} />
+      <Button value={6} />
+      <Button value="-" bg="orange" />
+      <Button value={1} />
+      <Button value={2} />
+      <Button value={3} />
+      <Button value="+" bg="orange" />
+      <Button value={0} span="span1" />
+      <Button value="." />
+      <Button value="=" bg="orange" />
+    </div>
+  );
 }
 
 export default Calculator;
