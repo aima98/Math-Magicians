8b08d30fc92a27fad21bb41f13c90c3577ddd6c9
Oumaima Nejjari <oumanj1998@gmail.com>
2023-04-27 17:48:50 +0100

    Consuming API and updating README file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Calculator from './components/calculator';
+import DisplayQuote from './components/displayQuote';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <Calculator />
+      <DisplayQuote />
+    </>
   );
 }
 
