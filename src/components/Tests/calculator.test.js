eac15714376c9e7282cf217093999948ed0e394b
Oumaima Nejjari <oumanj1998@gmail.com>
2023-05-03 22:09:33 +0100

    Fixing linters

--- src/components/Tests/calculator.test.js
+++ src/components/Tests/calculator.test.js
@@ -1,5 +1,5 @@
 import renderer from 'react-test-renderer';
-import Calculator from '../Calculator';
+import Calculator from '../calculator';
 
 it('renders calculator correctly', () => {
   const tree = renderer.create(
